bb6942f955e9066880f49493cc04b317c3b81f64
Colin Thornton <colinthorntontx@gmail.com>
2025-02-09 11:31:27 +0900

    Fix seeder

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -3,6 +3,6 @@ import User from '#models/user'
 
 export default class extends BaseSeeder {
   async run() {
-    await User.create({ email: 'test@example.com', password: 'test123' })
+    await User.create({ username: 'superadmin', password: 'test123' })
   }
 }
