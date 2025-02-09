2e99b6b3baee09505e1409c430a17564e3045fdc
Colin Thornton <colinthorntontx@gmail.com>
2025-02-09 10:55:50 +0900

    Show username on home page

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import { useForm } from '@inertiajs/vue3'
+
+defineProps<{
+  user: {
+    username: string
+  }
+}>()
 </script>
 
 <template>
-  <h1>Home</h1>
+  <h1>Hello, {{ user.username }}</h1>
   <form @submit.prevent="useForm({}).post('logout')"><button type="submit">Log Out</button></form>
 </template>
